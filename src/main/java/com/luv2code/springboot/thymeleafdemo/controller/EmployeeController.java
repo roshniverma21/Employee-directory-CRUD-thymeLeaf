d8802ead7eb2a106e3ce6fb0636c77736307a866
roshniverma21 <roshniverma021@gmail.com>
2024-07-06 12:52:09 +0530

    [Roshni] Add. save,update, delete endpoint

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -4,8 +4,7 @@ import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,7 +32,54 @@ public class EmployeeController {
 
         return "list-employees";
     }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model theModel) {
+
+        // create model attribute to bind form data
+        Employee theEmployee = new Employee();
+
+        theModel.addAttribute("employee", theEmployee);
+
+        return "employees/employee-form";
+    }
+
+    @PostMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int theId,
+                                    Model theModel) {
+
+        // get the employee from the service
+        Employee theEmployee = employeeService.findById(theId);
+
+        // set employee as a model attribute to pre-populate the form
+        theModel.addAttribute("employee", theEmployee);
+
+        // send over to our form
+        return "employees/employee-form";
+    }
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+
+        // save the employee
+        employeeService.save(theEmployee);
+
+        // use a redirect to prevent duplicate submissions
+        return "redirect:/employees/list";
+    }
+
+    @PostMapping("/delete")
+    public String delete(@RequestParam("employeeId") int theId) {
+
+        // delete the employee
+        employeeService.deleteById(theId);
+
+        // redirect to /employees/list
+        return "redirect:/employees/list";
+
+    }
 }
+
+
 
 
 
